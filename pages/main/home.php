9662838c5b3cd8f4b66e6688640932967e2f61c8
Bin65 <148638404+Bin65@users.noreply.github.com>
2024-05-07 00:08:30 +0700

    web ban sach

---
+++ pages/main/home.php
@@ -0,0 +1,166 @@
+<?php
+    
+?>
+<div>
+    <div id="slides" class="carousel slide" data-ride="carousel">
+        <ul class="carousel-indicators">
+          <li data-target="#slides" data-slide-to="0" class="active"></li>
+          <li data-target="#slides" data-slide-to="1"></li>
+          <li data-target="#slides" data-slide-to="2"></li>
+        </ul>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="intro-slide" src="./assets/images/banners/banner1.jpg" alt="banner">
+            <div class="carousel-caption d-none d-md-block">
+              
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img class="intro-slide" src="./assets/images/banners/banner2.jpg" alt="banner">
+            <div class="carousel-caption d-none d-md-block">
+              
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img class="intro-slide" src="./assets/images/banners/banner3.jpg" alt="banner">
+            <div class="carousel-caption d-none d-md-block">
+             
+            </div>
+          </div>
+        </div>
+      </div>
+      <section class="section-1">
+          <div class="container text-center">
+              <div class="row">
+                  <div class="col-md-6 col-12">
+                      <div class="pray">
+                          <img
+                              src="./assets/images/banners/banner4.jpg"
+                              alt="Pray"
+                              class=""
+                          />
+                      </div>
+                  </div>
+                  <div class="col-md-6 col-12">
+                      <div class="panel text-left">
+                          <h1>Phòng văn học</h1>
+                          <p class="pt-4">
+                          Chào mừng bạn đến với hiệu sách trực tuyến của chúng tôi, nơi thế giới sách trở nên sống động.
+                            Chúng tôi cung cấp một bộ sưu tập đá quý văn học đa dạng,
+                            từ những tác phẩm kinh điển vượt thời gian đến những cuốn sách bán chạy nhất đương đại.
+                          </p>
+                          <p>
+                          Khám phá, khám phá và để trí tưởng tượng của bạn bay cao.
+                            Với giao diện thân thiện với người dùng và đặt hàng dễ dàng,
+                            lần đọc tuyệt vời tiếp theo của bạn chỉ là một cú nhấp chuột.
+                            Hãy cùng chúng tôi tôn vinh sự kỳ diệu của sách.
+                          </p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </section>
+      <!-- Section 4 -->
+      <section class="section-4">
+          <div class="container text-center">
+              <h1 class="text-dark">Khách hàng nói gì về chúng tôi</h1>
+              <p class="font-italic">Phản hồi từ khách hàng thân thiết của chúng tôi</p>
+          </div>
+          <div class="team row">
+              <div class="col-md-4 col-12 text-center">
+                  <div class="card mr-2 d-inline-block shadow-lg">
+                      <div class="card-img-top">
+                          <img
+                              src="./assets/images/banners/feedback1.jpg"
+                              class="img-fluid p-4"
+                              alt=""
+                          />
+                      </div>
+                      <div class="card-body">
+                          <h3 class="card-title">Xuân Diệu</h3>
+                          <p class="card-text">
+                            "I'm really pleased with this website! The user interface is easy to navigate,
+                             and finding books is quick and simple. 
+                             I found my favorite books easily."
+                          </p>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-md-4 col-12">
+                  <div
+                      id="carouselExampleControls"
+                      class="carousel slide"
+                      data-ride="carousel"
+                  >
+                      <div class="carousel-inner text-center">
+                          <div class="carousel-item active">
+                              <div
+                                  class="card mr-2 d-inline-block shadow"
+                              >
+                                  <div class="card-img-top">
+                                      <img
+                                          src="./assets/images/banners/feedback2.jpg"
+                                          class="img-fluid p-4"
+                                          alt=""
+                                      />
+                                  </div>
+                                  <div class="card-body">
+                                      <h3 class="card-title">
+                                          Xuân Quỳnh
+                                      </h3>
+                                      <p class="card-text">
+                                      "Dịch vụ khách hàng tuyệt vời.
+                                      Họ đã giúp tôi giải quyết vấn đề với đơn đặt hàng của mình một cách nhanh chóng và chuyên nghiệp.
+                                      Tôi đánh giá cao tính chuyên nghiệp của họ."
+                                      </p>
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="carousel-item">
+                              <div
+                                  class="card d-inline-block mr-2 shadow"
+                              >
+                                  <div class="card-img-top">
+                                      <img
+                                          src="./assets/images/banners/feedback3.jpg"
+                                          class="img-fluid p-4"
+                                          alt=""
+                                      />
+                                  </div>
+                                  <div class="card-body">
+                                      <h3 class="card-title">
+                                         Tố Hữu
+                                      </h3>
+                                      <p class="card-text">
+                                      "Trang web này là giấc mơ của những người yêu sách. Nó dễ sử dụng,
+                                         cung cấp nhiều loại sách và giao hàng nhanh chóng.
+                                         Tôi là một khách hàng hạnh phúc!"
+                                      </p>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-md-4 col-12 text-center">
+                  <div class="card mr-2 d-inline-block shadow-lg">
+                      <div class="card-img-top">
+                          <img
+                              src="./assets/images/banners/feedback4.jpg"
+                              class="img-fluid p-4"
+                              alt=""
+                          />
+                      </div>
+                      <div class="card-body">
+                          <h3 class="card-title">Cao Bá Quát</h3>
+                          <p class="card-text">
+                          "Giao hàng nhanh và đóng gói cẩn thận.
+                          Sách đến trong tình trạng tốt, không bị hư hại gì.
+                          Chắc chắn sau này tôi sẽ quay lại mua sách ở đây."
+                          </p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </section>     
+</div>
